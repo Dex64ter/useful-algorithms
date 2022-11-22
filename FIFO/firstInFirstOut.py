5bfa91e159e0f8c54aa0f90af4979c1370f4281c
Davi J <davijosantos.dj@gmail.com>
2022-11-22 11:21:35 -0300

    att fifo

--- FIFO/firstInFirstOut.py
+++ FIFO/firstInFirstOut.py
@@ -1,30 +1,49 @@
-# from queue import Queue
-
-
 st1 = 'ESPERA'
 st2 = 'PRONTO'
 st3 = 'EXECUTANDO'
 st4 = 'TERMINADO'
 
+def removeAll(l, el):
+    result = []
+    for i in l:
+        if i != el:
+            result.append(i)
+    return result
+
+
 def PrimeiroAChegar(processos):
     timer = 0
     fila = []
     dicio_process = {}
-    status = []
-    for d in range(len(processos)):
-        dicio_process[d] = [processos[d][0],processos[d][1], st1]
-        status.append(st1)
+    status = [0]*len(processos)
     print(dicio_process)
 
     while True:
-        if st3 not in status:
-            for key in dicio_process.keys():
-                if dicio_process[key][2] == st1 and dicio_process[key][0] <= timer:
-                    dicio_process[key][2] = st2
-                    status[key] = st2
+        for d in range(len(processos)):
+            if processos[d][0] <= timer and status[d] != st3:
+                dicio_process[d] = [processos[d][0],processos[d][1], st2]
+                status[d] = st2
+
+        for k in dicio_process.keys():
+            if st3 not in status:
+                if dicio_process[k][2] == st2:
+                    dicio_process[k][2] = st3
+                    status[k] = st3
+            else:
+                for j in range(len(status)):
+                    if status[j] == st3:    
+                        fila.append(j)
+                        if fila.count(j) == dicio_process[j][1]:
+                            fila = removeAll(fila, j)
+                            dicio_process[j][2] = st4
+                            status[j] = st4
+                        break
+        print(dicio_process)
+        if (st2 not in status) or (0 not in status):
+            break
 
         timer += 1
-        break
+        
 
 
 if __name__ == "__main__":
